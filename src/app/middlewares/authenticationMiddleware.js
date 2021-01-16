4c21a61d6da218b337badc4998a08dc128248e9d
guilherme terriaga <guilherme.terriaga@gmail.com>
2021-01-16 16:53:38 -0300

    criado e configurado o middleware

---
+++ src/app/middlewares/authenticationMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConf from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token não enviado' });
+  }
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConf.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Token inválido' });
+  }
+};
