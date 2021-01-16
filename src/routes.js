70951250fe18b3480a5ce8ea4ab88e3600b1c8d1
guilherme terriaga <guilherme.terriaga@gmail.com>
2021-01-16 13:12:25 -0300

    criado estrutura do projeto e teste de rota

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+// import multerConfig from './config/multer';
+
+const routes = new Router();
+
+routes.get('/testezada', (req, res) => {
+  return res.json({ ok: true });
+});
